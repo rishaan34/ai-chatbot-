cb8de4b5fc3afa2e6dbc98dec47504b1b8b49ad9
rishaan34 <nandr9650@wrdsb.ca>
2025-04-08 12:16:11 -0400

    Rename jotform.css.9f939017.js to css.9f939017.js

---
+++ css.9f939017.js
@@ -0,0 +1,1 @@
+"use strict";(("undefined"==typeof self?this:self).chunkList=("undefined"==typeof self?this:self).chunkList||[]).push([["7222"],{60076:function(){},5499:function(){},11078:function(){}}]);
--- jotform.css.9f939017.js
+++
@@ -1,1 +0,0 @@
-"use strict";(("undefined"==typeof self?this:self).chunkList=("undefined"==typeof self?this:self).chunkList||[]).push([["7222"],{60076:function(){},5499:function(){},11078:function(){}}]);
